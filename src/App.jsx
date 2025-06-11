fcb7421617ea3b8cc2079379810a3e80f2beee1b
qsrdev <matteo.albertini747@gmail.com>
2025-06-11 16:19:46 +0200

    feat: form done

--- src/App.jsx
+++ src/App.jsx
@@ -19,26 +19,41 @@ function App() {
     <>
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-10">
+          <div className="col-8">
             <div className="card my-3">
               <div className="card-body">
-                <h3 className="text-center">Request Form</h3>
+                <h3 className="text-center">Aggiungi un post al blog</h3>
                 <form action="">
-                  <div class="mb-3">
-                    <label for="author" class="form-label">
-                      Author
+                  <div className="mb-3">
+                    <label for="author" className="form-label">
+                      Autore
                     </label>
-                    <input type="text" class="form-control" id="author" aria-describedby="author" placeholder="Matteo..." />
+                    <input type="text" className="form-control " id="author" aria-describedby="author" placeholder="Matteo..." />
                   </div>
-                  <div class="mb-3">
-                    <label for="title" class="form-label">
-                      Title
+                  <div className="mb-3">
+                    <label for="title" className="form-label">
+                      Titolo
                     </label>
-                    <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Ho bisogno di 4 mozzarelle..." />
-                    <div id="title" class="form-text">
-                      Insert the title of your request
+                    <input type="text" className="form-control" id="title" aria-describedby="title" placeholder="Ho bisogno di 4 mozzarelle..." />
+                    <div id="title" className="form-text">
+                      Inserisci il titolo del post
                     </div>
                   </div>
+                  <div className="mb-3">
+                    <label for="body" className="form-label">
+                      Inserisci il testo del post
+                    </label>
+                    <textarea className="form-control" id="body" rows="3" placeholder="specifica la tua richiesta..."></textarea>
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="public" />
+                    <label className="form-check-label" for="public">
+                      Spuntare per pubblicare il post
+                    </label>
+                  </div>
+                  <button type="submit" class="btn btn-success">
+                    Pubblica
+                  </button>
                 </form>
               </div>
             </div>
